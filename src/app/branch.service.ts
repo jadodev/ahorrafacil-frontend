0e01a85f6da723109592634e45126b25d77cefaa
Jonathan Urrego <jhonnathan.dev@gmail.com>
2024-10-12 23:30:10 -0500

    fix enviroment path

--- src/app/branch.service.ts
+++ src/app/branch.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { environment } from '../../enviroment';
+import { environment } from '../enviroment';
 
 @Injectable({
   providedIn: 'root'
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { environment } from '../../../enviroment';
+import { environment } from '../../enviroment';
 
 @Injectable({
   providedIn: 'root'
